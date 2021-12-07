28f3cbcb6f147f123fbb6c177d179f5ae92f21c7
Liam <liampoole02@gmail.com>
2021-12-07 22:49:47 +0200

    added delete functionality

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -112,6 +112,10 @@ class TodosController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $todo=Todo::find($id);
+
+        $todo->delete();
+
+        return redirect('/')->with('success', 'Todo deleted successfully!');
     }
 }
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -6,5 +6,14 @@
 <span class="badge bg-secondary">{{ $todo->due }}</span>
 <hr>
 <p>{{ $todo->content }}</p>
+
+<form method="post" action="/todos/{{ $todo->id }}">
+    @csrf
+    @method('DELETE')
+<button type="submit" class="btn btn-danger mt-2 float-right">Delete</button>
+</form>
+
 <a href="/todos/{{ $todo->id }}/edit" class="btn btn-primary mt-2">Edit</a>
+
+
 @endsection

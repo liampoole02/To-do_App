9e9adad2cc5d886fb1cf2d3ee2775b4f0805b7c5
Liam <liampoole02@gmail.com>
2021-12-07 15:33:47 +0200

    Created to-do index view

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Todo List</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet"></head>
+
+<body>
+    <div>
+        @yield('content')
+    </div>
+
+</body>
+<footer class="text-center">Copyright 2021</footer>
+
+</html>
